1f8fd7471abe739d33e09a4ed3bab0d12e786c7f
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2025-01-04 15:32:10 +0530

    fix: update API key handling in chatbot.py for consistency

--- chatbot.py
+++ chatbot.py
@@ -49,7 +49,7 @@ def split_and_save_docs(docs):
 
 
 def get_vector_store(chunks):
-    embeddings = OpenAIEmbeddings(api_key=st.write("openai_api_key"))
+    embeddings = OpenAIEmbeddings(api_key=st.write("OPENAI_API_KEY"))
     vectorstore = Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
@@ -62,7 +62,7 @@ def conversational_chain(retriever):
     prompt = hub.pull("rlm/rag-prompt")
 
     llm = ChatOpenAI(
-        temperature=0.1, model="gpt-4o", api_key=st.write("openai_api_key")
+        temperature=0.1, model="gpt-4o", api_key=st.write("OPENAI_API_KEY")
     )
     rag_chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
